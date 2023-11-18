9abde993092c79ab8de32fd0581ceff452e92aa7
九微 <75103805+jivei@users.noreply.github.com>
2023-11-18 16:57:38 +0800

    更新 Documents.js

--- js/Documents.js
+++ js/Documents.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/license\.pdfexpert\.com\/api\/2\.0\/documents\/subscription\/refresh url script-request-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Documents.js
+^https:\/\/license\.pdfexpert\.com\/api\/2\.0\/documents\/subscription\/refresh url script-request-body https://raw.githubusercontent.com/jivei/Surge/main/js/Documents.js
 
 [mitm]
 hostname = license.pdfexpert.com
