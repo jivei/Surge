76e5fe740561fceef0cc3f0a26072f8b632c786c
九微 <75103805+jivei@users.noreply.github.com>
2024-05-14 15:50:11 +0800

    Create test.js

---
+++ js/test.js
@@ -0,0 +1,12 @@
+[rewrite_local]
+^http:\/\/app\.ddpai\.com\/onroad\/api\/v1\/news\/list url reject-dict
+^http:\/\/app\.ddpai\.com\/onroad\/api\/v2\/activity\/list url reject-dict
+^http:\/\/app\.ddpai\.com\/onroad\/api\/v1\/activity\/list url reject-dict
+^http:\/\/app\.ddpai\.com\/d\/api\/v1\/grade\/privilege url reject-dict
+
+^http:\/\/app\.ddpai\.com\/bigdata\/api\/v1\/data\/upload url reject-dict
+^http:\/\/app\.ddpai\.com\/onroad\/api\/v2\/banner\/list url reject-dict
+^http:\/\/app\.ddpai\.com\/onroad\/api\/v1\/story\/list url reject-dict
+^http:\/\/appgw\.ddpai\.com:18080\/ddpai\/api\/v2\/quality\/card\/list url reject-dict
+^http:\/\/appgw\.ddpai\.com:18080\/market\/api\/v1\/ware\/getWareDetail url reject-dict
+^http:\/\/appgw\.ddpai\.com:18080\/onroad\/api\/v1\/ad\/list url reject-dict
